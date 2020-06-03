a3f47d81cf23ed2a4980380913336ff20c39ea45
nvsmle <alexandr-super@mail.ru>
2020-06-03 11:46:14 +0700

    completed app

--- app/src/main/java/neversmile/packag/com/instagramcloneapp/UsersTab.java
+++ app/src/main/java/neversmile/packag/com/instagramcloneapp/UsersTab.java
@@ -12,8 +12,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -21,11 +23,14 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UsersTab extends Fragment implements AdapterView.OnItemClickListener {
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView listView;
     private ArrayList<String> arrayList;
@@ -47,6 +52,7 @@ public class UsersTab extends Fragment implements AdapterView.OnItemClickListene
         arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
 
         listView.setOnItemClickListener(UsersTab.this);
+        listView.setOnItemLongClickListener(UsersTab.this);
 
         final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
 
@@ -88,5 +94,45 @@ public class UsersTab extends Fragment implements AdapterView.OnItemClickListene
         intent.putExtra("username", arrayList.get(position));
         startActivity(intent);
 
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", arrayList.get(position));
+
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if (user != null && e == null) {
+
+                    final PrettyDialog prettyDialog = new PrettyDialog(getContext());
+
+                    prettyDialog.setTitle(user.getUsername() + "'s Info")
+                        .setMessage(user.get("profileBio") + "\n"
+                                + user.get("profileProfession") + "\n"
+                                + user.get("profileHobbies") + "\n"
+                                + user.get("profileFavoriteSport"))
+                        .setIcon(R.drawable.person)
+                        .addButton("OK",
+                                R.color.pdlg_color_white,
+                                R.color.pdlg_color_green,
+                                new PrettyDialogCallback() {
+                                    @Override
+                                    public void onClick() {
+                                        prettyDialog.dismiss();
+                                    }
+                                }).show();
+
+
+
+                }
+
+            }
+        });
+
+        return true;
     }
 }
